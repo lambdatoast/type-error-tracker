232fee772fc13ce0392b518e2f8da3353514bfea
Alexander Noriega <lambdatoast@gmail.com>
2015-12-20 10:39:40 -0300

    Add app.ts

---
+++ src/app/app.ts
@@ -0,0 +1,85 @@
+/// <reference path="../definitions/cordova/cordova.d.ts" />
+/// <reference path="../definitions/cordova-ionic/cordova-ionic.d.ts" />
+
+'use strict';
+
+angular.module('starter', ['ionic', 'chart.js', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      window.StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.errors', {
+    url: '/errors',
+    views: {
+      'tab-errors': {
+        templateUrl: 'templates/tab-errors.html',
+        controller: 'ErrorsCtrl'
+      }
+    }
+  })
+
+  .state('tab.history', {
+    url: '/history',
+    views: {
+      'tab-history': {
+        templateUrl: 'templates/tab-history.html',
+        controller: 'HistoryCtrl'
+      }
+    }
+  })
+
+  .state('tab.history-edit', {
+    url: '/history/edit/:id',
+    resolve: {
+      error: function ($stateParams, Error) {
+        return Error.getById($stateParams.id);
+      }
+    },
+    views: {
+      'tab-history': {
+        templateUrl: 'templates/tab-errors-edit.html',
+        controller: 'ErrorsEditCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
