eabc605d63c9c5ddff122702cbfbf71ec2f3c157
Alexander Noriega <lambdatoast@gmail.com>
2015-12-19 23:05:52 -0300

    Add typescript config and create some types.

---
+++ src/app/services.ts
@@ -0,0 +1,117 @@
+/// <reference path="../definitions/angularjs/angular.d.ts" />
+
+interface TE {
+    type: string;
+    cost: number;
+    unit: string;
+    typeSystemFeatures: any;
+}
+
+interface ErrorModule {
+  all: () => any,
+  save: (error: TE) => any,
+  clear: () => any,
+  remove: () => any,
+  update: () => any,
+  getById: () => any,
+  typeSystemFeatures: Object,
+  is: () => any
+};
+
+angular.module('starter.services', [])
+
+.factory('moment', function($window) {
+  return $window.moment;
+})
+
+.factory('Error', function($window) {
+
+  function save(error) {
+    error.id = +(new Date());
+    var errors = load();
+    errors.push(error);
+    $window.localStorage.setItem('errors', JSON.stringify(errors));
+  }
+
+  function load() {
+    try {
+      var xs = JSON.parse($window.localStorage.getItem('errors'));
+      return xs ? xs : [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  function getById(id) {
+    var xs = load().filter(function (e) {
+      return e.id+'' === id+'';
+    });
+    if (xs.length) {
+      return xs[0];
+    } else {
+      throw new Error('Did not find error with ID of: ' + id);
+    }
+  }
+
+  function clear() {
+    $window.localStorage.removeItem('errors');
+  }
+
+  function remove(error) {
+    var errors = load().filter(function (e) {
+      return e.id !== error.id;
+    });
+    $window.localStorage.setItem('errors', JSON.stringify(errors));
+  }
+
+  function update(newError) {
+    var errors = load().map(function (e) {
+      return e.id === newError.id ? newError : e;
+    });
+    $window.localStorage.setItem('errors', JSON.stringify(errors));
+  }
+
+  var typeSystemFeatures = {
+    nominal: { checked: false },
+    structural: { checked: false },
+    polymorphic: { checked: false },
+    existential: { checked: false },
+    phantom: {checked: false },
+    liquid: { checked: false },
+    dependent: { checked: false },
+    linear: { checked: false }
+  };
+
+  function is(type) { return function (error) { return error.type === type; }; }
+
+  return {
+    all: load,
+    save: save,
+    clear: clear,
+    remove: remove,
+    update: update,
+    getById: getById,
+    typeSystemFeatures: typeSystemFeatures,
+    is: is
+  };
+
+})
+
+.factory('Cost', function(Error) {
+  // [Error] -> String
+  function totalHours(errors) {
+    return Number(errors.reduce(function (acc, err) {
+      var cost = Number(err.cost);
+      return acc + (err.unit === 'hrs' ? cost : cost/60);
+    }, 0)).toFixed(2);
+  }
+  function totals(errors) {
+    return {
+      Type: totalHours(errors.filter(Error.is('Type'))),
+      Other: totalHours(errors.filter(Error.is('Other')))
+    };
+  }
+  return {
+    totals: totals
+  };
+})
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,129 +1,106 @@
 angular.module('starter.controllers', [])
-
-.controller('DashCtrl', function($scope, Error, Cost) {
-  $scope.$on('$ionicView.enter', function(e) {
-    $scope.errors = Error.all();
-    $scope.errorCount = $scope.errors.length;
-    $scope.hours = Cost.totals($scope.errors);
-    var typeSystemFeaturesDemand = $scope.errors.filter(Error.is('Type')).reduce(function (acc, e) {
-      return Object.keys(e.typeSystemFeatures).reduce(function (acc, feature) {
-        acc[feature] = acc[feature] !== undefined ? acc[feature] : 0;
-        acc[feature] = acc[feature] + (e.typeSystemFeatures[feature].checked === true ? 1 : 0);
-        return acc;
-      }, acc);
-    }, {});
-    $scope.typeSystemFeaturesDemand = Object.keys(typeSystemFeaturesDemand).reduce(function (acc, feature) {
-      acc.labels = acc.labels.concat([feature.toUpperCase().slice(0,1) + feature.slice(1) + ' types']);
-      acc.data = acc.data.concat([typeSystemFeaturesDemand[feature]]);
-      return acc;
-    }, {labels: [], data: []});
-    $scope.showTypeSystemFeaturesDemand = $scope.typeSystemFeaturesDemand.data.some(function (n) { return n > 0; });
-  });
+    .controller('DashCtrl', function ($scope, Error, Cost) {
+    $scope.$on('$ionicView.enter', function (e) {
+        $scope.errors = Error.all();
+        $scope.errorCount = $scope.errors.length;
+        $scope.hours = Cost.totals($scope.errors);
+        var typeSystemFeaturesDemand = $scope.errors.filter(Error.is('Type')).reduce(function (acc, e) {
+            return Object.keys(e.typeSystemFeatures).reduce(function (acc, feature) {
+                acc[feature] = acc[feature] !== undefined ? acc[feature] : 0;
+                acc[feature] = acc[feature] + (e.typeSystemFeatures[feature].checked === true ? 1 : 0);
+                return acc;
+            }, acc);
+        }, {});
+        $scope.typeSystemFeaturesDemand = Object.keys(typeSystemFeaturesDemand).reduce(function (acc, feature) {
+            acc.labels = acc.labels.concat([feature.toUpperCase().slice(0, 1) + feature.slice(1) + ' types']);
+            acc.data = acc.data.concat([typeSystemFeaturesDemand[feature]]);
+            return acc;
+        }, { labels: [], data: [] });
+        $scope.showTypeSystemFeaturesDemand = $scope.typeSystemFeaturesDemand.data.some(function (n) { return n > 0; });
+    });
 })
-
-.controller('ErrorsCtrl', function($scope, $state, Error, $ionicPopup) {
-
-  $scope.newError = {
-    type: 'Type',
-    cost: 1,
-    unit: 'min',
-    typeSystemFeatures: Error.typeSystemFeatures
-  };
-
-  $scope.add = function () {
-    Error.save({
-      type: $scope.newError.type, 
-      cost: $scope.newError.cost,
-      unit: $scope.newError.unit,
-      typeSystemFeatures: $scope.newError.typeSystemFeatures
-    });
-    $state.go('tab.history');
-  };
-
-  $scope.clear = function () {
-    var confirmPopup = $ionicPopup.confirm({
-      title: 'Clear records',
-      template: 'Are you sure you want to remove all records?'
-    });
-    confirmPopup.then(function(res) {
-      if(res) {
-        Error.clear();
-      } else {
-      }
-    });
-  };
-
-  var sounds = {
-    kick: new Howl({
-      urls: ['audio/timpani.wav']
-    }),
-    snare: new Howl({
-      urls: ['audio/snare.wav']
-    })
-  };
-
-  $scope.note = function (name) {
-    sounds[name].play();
-  };
-
+    .controller('ErrorsCtrl', function ($scope, $state, Error, $ionicPopup) {
+    var newError = {
+        type: 'Type',
+        cost: 1,
+        unit: 'min',
+        typeSystemFeatures: Error.typeSystemFeatures
+    };
+    $scope.newError = newError;
+    $scope.add = function () {
+        Error.save({
+            type: $scope.newError.type,
+            cost: $scope.newError.cost,
+            unit: $scope.newError.unit,
+            typeSystemFeatures: $scope.newError.typeSystemFeatures
+        });
+        $state.go('tab.history');
+    };
+    $scope.clear = function () {
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Clear records',
+            template: 'Are you sure you want to remove all records?'
+        });
+        confirmPopup.then(function (res) {
+            if (res) {
+                Error.clear();
+            }
+            else {
+            }
+        });
+    };
 })
-
-.controller('ErrorsEditCtrl', function($scope, $state, error, Error) {
-  $scope.error = error;
-  $scope.typeSystemFeatures = Object.keys(error.typeSystemFeatures);
-  $scope.update = function () {
-    Error.update($scope.error);
-    $state.go('tab.history');
-  };
+    .controller('ErrorsEditCtrl', function ($scope, $state, error, Error) {
+    $scope.error = error;
+    $scope.typeSystemFeatures = Object.keys(error.typeSystemFeatures);
+    $scope.update = function () {
+        Error.update($scope.error);
+        $state.go('tab.history');
+    };
 })
-
-.controller('HistoryCtrl', function($scope, $state, Error, Cost, $ionicPopup, moment) {
-  $scope.errors = [];
-  $scope.history = [];
-
-  function init() {
-    $scope.errors = Error.all().concat([]).reverse();
-    $scope.history = createHistory($scope.errors);
-  }
-
-  function createHistory(errors) {
-    var FORMAT = 'YYYY-MM-DD';
-    var map = errors.reduce(function (acc, e) {
-      var day = moment(new Date(e.id)).format(FORMAT);
-      acc[day] = acc[day] ? acc[day].concat([e]) : [e];
-      return acc;
-    }, {});
-    return Object.keys(map).sort(function (date1, date2) {
-      return moment(date1, FORMAT).isBefore(moment(date2, FORMAT)) ? -1 : 1;
-    }).map(function (k) {
-      var costs = Cost.totals(map[k]);
-      return { 
-        day: k, 
-        errors: map[k], 
-        //costs: Cost.totals(map[k])
-        costs: Object.keys(costs).reduce(function (acc, k) {
-          return acc.concat([{type: k, cost: costs[k]}]);
-        }, [])
-      };
-    }).reverse();
-  }
-
-  $scope.$on('$ionicView.enter', function(e) {
-    init();
-  });
-
-  $scope.remove = function (error) {
-    var confirmPopup = $ionicPopup.confirm({
-      title: 'Remove',
-      template: 'Are you sure you want to remove this record?'
-    });
-    confirmPopup.then(function(res) {
-      if(res) {
-        Error.remove(error);
+    .controller('HistoryCtrl', function ($scope, $state, Error, Cost, $ionicPopup, moment) {
+    $scope.errors = [];
+    $scope.history = [];
+    function init() {
+        $scope.errors = Error.all().concat([]).reverse();
+        $scope.history = createHistory($scope.errors);
+    }
+    function createHistory(errors) {
+        var FORMAT = 'YYYY-MM-DD';
+        var map = errors.reduce(function (acc, e) {
+            var day = moment(new Date(e.id)).format(FORMAT);
+            acc[day] = acc[day] ? acc[day].concat([e]) : [e];
+            return acc;
+        }, {});
+        return Object.keys(map).sort(function (date1, date2) {
+            return moment(date1, FORMAT).isBefore(moment(date2, FORMAT)) ? -1 : 1;
+        }).map(function (k) {
+            var costs = Cost.totals(map[k]);
+            return {
+                day: k,
+                errors: map[k],
+                //costs: Cost.totals(map[k])
+                costs: Object.keys(costs).reduce(function (acc, k) {
+                    return acc.concat([{ type: k, cost: costs[k] }]);
+                }, [])
+            };
+        }).reverse();
+    }
+    $scope.$on('$ionicView.enter', function (e) {
         init();
-      } else {
-      }
     });
-  };
-
+    $scope.remove = function (error) {
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Remove',
+            template: 'Are you sure you want to remove this record?'
+        });
+        confirmPopup.then(function (res) {
+            if (res) {
+                Error.remove(error);
+                init();
+            }
+            else {
+            }
+        });
+    };
 });
